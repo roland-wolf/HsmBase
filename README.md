1b09efda8e07088c5bd8d62b0f38222e6f5eb786
Roland Wolf <rolrolrol@web.de>
2015-04-11 23:39:42 +0200

    first version of HsmBase

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # HsmBase
-A type safe hierarchical state machine for C++
+A type safe hierarchical state machine implementation for C++ 
+
+While there are many options to add a hierarchical state machine (HSM) to your project all of them have shortcomings in certain scenarios:
+
+- There are free and commercial generators which take a HSM description and produce C/C++ code.
+- boost comes with 2 different HSM. These state machines make heavy use of template expressions.
+- Miro Samek published HSM software. He associates states with methods. 
+
+Here is another option: Derive from HsmBase and implement every state as a method. This direct mapping between bubbles in your state chart and methods in your code makes development and maintaining of HSM's easy. The way you write state machines with HsmBase is similar Miro Samek's state machine. 
+
+## Features of HsmBase
+
+- type safe, no makros
+- free for commercial and non-commercial use 
+- runs very fast
+- compiles very fast, even for huge HSMs
+- type safe
+- number of states and nesting depth is not limited
+- events can be enums or of class type - values or pointers
+- needs a c++03 compliant compiler and has no further depedency on libraries
+- supports inits, entry / exit actions and transitions.  Transitions will be called in the correct order.
+- HsmBase is a template but the state machine class you write is not.
+- No template expressions, very straightforward code. 
+
+
+Currently there is no documentation but a tiny example. 
+
+The general usage is simple: Copy the file stateprocessor.h to your project. Derive a class from stm::StateProcessor which needs two template parameters. The first template parameter is the derived class (google for CRTP if this feels odd), the second parameter ist your event type.  
---
+++ example/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include "../src/stateprocessor.h"
+
+
+using namespace std;
+
+
+
+enum Event {
+    Connect, Disconnect, SessionReceived, SessionLost
+};
+
+class Stm : public stm::StateProcessor<Stm, Event>
+{
+public:
+    Stm();
+
+private:
+    void init(const Event *ev);
+    void root(const Event *ev){}
+    void connected(const Event *ev);
+    void disconnected(const Event *ev);
+    void hasSession(const Event *ev);
+};
+
+Stm::Stm(){
+    setRoot(&Stm::root);
+    addChildState(&Stm::root, &Stm::connected);
+    addChildState(&Stm::root, &Stm::disconnected);
+    addChildState(&Stm::connected, &Stm::hasSession);
+    initStatemachine(&Stm::disconnected, &Stm::init);
+}
+
+void Stm::init(const Event *ev)
+{
+    cout << "state machine was started, greetings from initial transition" << endl;
+}
+
+void Stm::connected(const Event *ev)
+{
+    switch(reason()){
+    case Stm::ENTRY:
+        cout << "connected Entry" << endl;
+        break;
+    case Stm::EXIT:
+        cout << "connected Exit" << endl;
+        break;
+    case Stm::INIT:
+        cout << "connected init" << endl;
+        break;
+    case Stm::ACTION:
+        cout << "connected action" << endl;
+        break;
+    case Stm::ROUTE:
+        cout << "connected Route" << endl;
+        if( *ev == Disconnect){
+            transition(&Stm::disconnected);
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+void Stm::disconnected(const Event *ev)
+{
+    switch(reason()){
+    case Stm::ENTRY:
+        cout << "discon Entry" << endl;
+        break;
+    case Stm::EXIT:
+        cout << "discon Exit" << endl;
+        break;
+    case Stm::INIT:
+        cout << "discon init" << endl;
+        break;
+    case Stm::ACTION:
+        cout << "discon action" << endl;
+        break;
+    case Stm::ROUTE:
+        cout << "discon Route" << endl;
+        if( *ev == Connect){
+            transition(&Stm::connected);
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+void Stm::hasSession(const Event *ev)
+{
+    if( *ev == Connect){
+        transition(&Stm::disconnected);
+    }
+}
+
+int main()
+{
+    Stm sm;
+    Event evConnect = Connect;
+    sm.start();
+    sm.processEvent(&evConnect);
+    Event evDisconnect = Disconnect;
+    sm.processEvent(&evDisconnect);
+    sm.processEvent(&evDisconnect);
+    return 0;
+}
+
+
+
